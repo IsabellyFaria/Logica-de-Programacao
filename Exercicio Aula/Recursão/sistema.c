f3a26d62e97acca59b0f4f406909e58fb0afe5c0
Isabelly Faria <isabelly.faria06@gmail.com>
2026-06-10 14:37:45 +0000

    Sistema de manipulação de turma

--- Exercicio Aula/Recursão/sistema.c
+++ Exercicio Aula/Recursão/sistema.c
@@ -9,37 +9,68 @@ typedef struct{
     int frequencia;
 }Aluno;
 
-void imprimirMenu()
-int buscaRA(Aluno turma[], int n, int RA)
-void insereAluno(Aluno turma[], int n, int RA)
-void imprimeTurma(Aluno turma[], int n)
+void imprimirMenu();
+int buscaRA(Aluno turma[], int n, int RA);
+void insereAluno(Aluno turma[], int n, int RA);
+void imprimeTurma(Aluno turma[], int n);
+void imprimeAluno(Aluno turma[], int i);
+void removeAluno(Aluno turma[], int n, int p);
+int getRA();
 
 int main(){
     Aluno turmaLP[50];
-    int op, n =0;
-    system("clear");
-    imprimirMenu();
-    scanf("%d", &op);
-    
-    switch(op){
-        case 1:
-        if(n == 50){
-            printf("\nTurma Lotada.");
-        }else{
-            printf("\nRA: ");
-            scanf("%d", &RA);
-            if(!buscaRA(turmaLP,n, RA)){
-                insereAluno(turmaLP,n,RA);
-                n++;
-            }
-        }
-        case 2:
-            if(n==0){
-                printf("Turma vazia!");
-            }else{
+    int op=-1, n = 0,RA,posicao;
 
-            }
+    while(op != 5){
+        system("clear");
+        imprimirMenu();
+        scanf("%d", &op);
+        
+        switch(op){
+            case 1:
+                if(n == 50){
+                    printf("\nTurma Lotada.");
+                }else{
+                    RA = getRA();
+                    if(buscaRA(turmaLP,n, RA)==-1){
+                        insereAluno(turmaLP,n,RA);
+                        n++;
+                    }
+                }
+                break;
+            case 2:
+                if(n==0){
+                    printf("Turma vazia!");
+                }else{
+                    imprimeTurma(turmaLP,n);
+                }
+                break;
+            case 3:
+                RA = getRA();
+                posicao = buscaRA(turmaLP, n, RA); 
+                if(posicao != -1){
+                    imprimeAluno(turmaLP, posicao);
+                }else{
+                    printf("Aluno não encontrado!");
+                }
+                break;
+            case 4:
+                RA = getRA();
+                posicao = buscaRA(turmaLP, n, RA); 
+                if(posicao != -1){
+                    removeAluno(turmaLP, n, posicao);
+                    n--;
+                }else{
+                    printf("Aluno não encontrado!");
+                }
+                break;
+        }
+
+        printf("\n\n\n\n\n... Aperte ENTER para continuar ...");
+        while(getchar() != '\n');
+        getchar();
     }
+    
 
     return 0;
 }
@@ -49,18 +80,20 @@ void imprimirMenu(){
     printf("\n2 - Imprime Turma");
     printf("\n3 - Busca Aluno Por RA");
     printf("\n4 - Remove Aluno");
-    printf("\n5 - Finalizar");
+    printf("\n5 - Finalizar\n\n");
 }
+
 int buscaRA(Aluno turma[], int n, int RA){
     for(int i = 0; i < n; i++){
         if(turma[i].RA == RA){
-            return 1;
+            return i;
         }
     }
-    return 0;
+    return -1;
 }
+
 void insereAluno(Aluno turma[], int n, int RA){
-    printf("\nDIgite o nome: ");
+    printf("\nDigite o nome: ");
     getchar();
     fgets(turma[n].nome, sizeof(turma[n].nome), stdin);
     turma[n].nome[strlen(turma[n].nome)-1] = '\0';
@@ -68,7 +101,7 @@ void insereAluno(Aluno turma[], int n, int RA){
     for(int i = 0; i < 3; i++){
         turma[n].notas[i] = -1;
         while(turma[n].notas[i] < 0){
-           printf("Digite a nota da P%i", i+1);
+           printf("\nDigite a nota da P%i: ", i+1);
             scanf("%f",&turma[n].notas[i]); 
         }
     }
@@ -76,9 +109,34 @@ void insereAluno(Aluno turma[], int n, int RA){
     scanf("%d", &turma[n].frequencia);
     turma[n].RA = RA;
 }
+
 void imprimeTurma(Aluno turma[], int n){
     for(int i = 0; i < n; i++){
-        printf("\nAluno %i",i+1);
-        printf("\tNome: %s", alun)
+        imprimeAluno(turma, i);
+    }
+}
+
+void imprimeAluno(Aluno turma[], int i){
+    printf("\nAluno %i",i+1);
+    printf("\tNome: %s", turma[i].nome);
+    printf("\tRA: %i", turma[i].RA);
+    for(int j = 0; j < 3; j++){
+       printf("\tNota P%i:  %.2f", j+1, turma[i].notas[j]);
+    }
+    printf("\tFrequência: %i", turma[i].frequencia);
+}
+
+int getRA(){
+    int RA = 0;
+    while(RA == 0){
+        printf("\nDigite o RA: ");
+        scanf("%i", &RA);
+    }
+    return RA;
+}
+
+void removeAluno(Aluno turma[], int n, int p){
+    for(int i = p; i<n;i++){
+        turma[i] = turma[i+1];
     }
 }
